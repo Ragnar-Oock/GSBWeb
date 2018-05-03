c0bc50507af943b0d09bd6fb85da7191e85ae093
smqdes <38745703+smqdes@users.noreply.github.com>
2018-05-03 17:56:37 +0200

    Fichier adaptés à la base de données et à la table Praticien au lieu de Agents

--- test/vues/v_choixPraticiens.php
+++ test/vues/v_choixPraticiens.php
@@ -1,4 +1,4 @@
-<!-- choix d'un Agent / Derniere modification le 6 février 2014 par Pascal Blain -->
+<!-- choix d'un Praticien / Derniere modification le 3 Mai 2018 par Théo Corbel   -->
 <?php
 	$nbL=count($lesLignes); 
 	echo ' 
@@ -29,12 +29,12 @@
 		        <input type="image"	id="zNouveau" 	title="Ajouter" 		src="images/ajout.gif" 			onclick="faire('choixP', 'ajouter')">
                 <input type="image"	id="zModif" 		title="Modifier" 		src="images/modif.gif" 			onclick="faire('choixP', 'modifier')">
                 <input type="image"	id="zSupprime" 	title="Supprimer"	src="images/supprimer.gif" 		onclick="faire('choixP', 'supprimer')">&nbsp;&nbsp;
-                <input type="image"	id="zPremier" 	title="premier" 		src="images/goPremier.gif" 		onclick="premier('choixP','lstAgents')">    
-		        <input type="image" id="zPrecedent" 	title="pr&eacute;c&eacute;dent" src="images/goPrecedent.gif" onclick="precedent('choixP','lstAgents')"> 
+                <input type="image"	id="zPremier" 	title="premier" 		src="images/goPremier.gif" 		onclick="premier('choixP','lstPraticiens')">    
+		        <input type="image" id="zPrecedent" 	title="pr&eacute;c&eacute;dent" src="images/goPrecedent.gif" onclick="precedent('choixP','lstPraticiens')"> 
 <?php echo '	
 				<input type="text" 	id="zNumero" value="'.$noL.'/'.$nbL.'" disabled="true" size="5" style="text-align:center;vertical-align:top;">'; ?>
-		        <input type="image" 	id="zSuivant" 	title="suivant" 		src="images/goSuivant.gif" 		onclick="suivant('choixP','lstAgents')">    
-		        <input type="image"	id="zDernier" 		title="dernier" 		src="images/goDernier.gif" 		onclick="dernier('choixP','lstAgents')">    
+		        <input type="image" 	id="zSuivant" 	title="suivant" 		src="images/goSuivant.gif" 		onclick="suivant('choixP','lstPraticiens')">    
+		        <input type="image"	id="zDernier" 		title="dernier" 		src="images/goDernier.gif" 		onclick="dernier('choixP','lstPraticiens')">    
 		    </div>
             <input type="hidden"		name="action"			value="<?php if($_REQUEST['action']=="liste") {echo "voir";} else {echo $_REQUEST['action'];}?>">
             <input type="hidden"		name="type" 				value="<?php echo $_REQUEST['type']?>">
