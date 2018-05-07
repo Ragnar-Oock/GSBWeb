21b0a4bad66aa080c5028af2d094ea808fff9074
UgoDumontot <38219333+UgoDumontot@users.noreply.github.com>
2018-05-07 18:02:54 +0200

    update the search bar
    make it works

--- test/vues/v_afficherRecherche.php
+++ test/vues/v_afficherRecherche.php
@@ -1,7 +1,7 @@
 <?php
-/*echo("
+echo("
 	<div>
-		Il y a :".count($results));
+		Il y a : ".count($results));
 if(count($results)>1)
 {
 	echo(" résultats.");
@@ -10,12 +10,26 @@ else
 {
 	echo(" résultat.");
 }
-echo("</div>");*/
+echo("</div>");
 
 foreach ($results as $unResultat)
 {
-	echo('<div class="resultatRecherche"><h3>'.$unResultat["vRapport"].'</h3>'.$unResultat["vMotif"].'</div>');
+	echo('
+		<div>
+			<table style="margin: 5px 0px 0px 0px;
+  border-radius: 5px;
+  color: #ffffff;
+  background-color: #90a4ae;">
+				<tr style="width: 100%">
+					<td style="width: 75%">'.$unResultat["vMotif"].'</td>
+					<td style="width: 25%">'.$unResultat["vDate"].'</td>
+				<tr style="width: 100%">
+					<td>Rapport : '.$unResultat["vRapport"].'</td>
+				<tr style="width: 100%">
+					<td style="width: 50%">Praticien : '.$unResultat["pNom"].' '.$unResultat["pPrenom"].'</td>
+					<td style="width: 50%">Visteur : '.$unResultat["uNom"].' '.$unResultat["uPrenom"].'</td>
+			</table>
+		</div>
+		');
 }
-
-
 ?>
--- test/vues/v_recherche.php
+++ test/vues/v_recherche.php
@@ -1,10 +1,26 @@
 <!--affichage de la barre de recherche-->
 <?php
 	$titre="rechercher";
-	echo (' <form action="index.php" method="GET">
-				<input type="hidden" name="choixTraitement" value="recherche"/>
-				<input type="hidden" name="action" value="voir"/>
-				<input type="text" placeholder="rechercher" name="search"/>
-				<input type="submit" value="Rechercher"/>
-			</form>');
 ?>
+<form name="frmSearch" action="index.php" method="GET">
+	<input type="hidden" name="choixTraitement" value="recherche"/>
+	<input type="hidden" name="action" value="voir"/>
+	<input type="text" placeholder="rechercher" name="recherche"/>
+	<input type="button" value="rechercher" onclick="longueurRecherche('frmSearch','recherche','3')">
+	<!--<a href="javascript: longueurRecherche('frmSearch','recherche','3')">rechercher</a>-->
+</form>
+<script type="text/javascript">
+	function longueurRecherche(frm, champ, longueur)
+	{
+		//alert(document.forms[frm].elements[champ].value.length);
+		if(document.forms[frm].elements[champ].value.length >= longueur)
+		{
+			document.forms[frm].submit();
+			//alert(document.forms[frm].elements[champ].value.length);
+		}
+		else{
+			alert("Faites une recherche d'au moins trois caracteres.");
+		}
+	}
+</script>
+<!--<input type="button" value="recherche" onclick="longueurRecherche(\'frmSearch\',\'recherche,\'3\')"/>-->
